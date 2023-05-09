b6786460d6ece9cf16742b59d45bfc97d41ac509
pedrobarretto <pedro@barretto.com.br>
2023-05-08 21:50:53 -0300

    feat: first sintax success

--- sintatico.c
+++ sintatico.c
@@ -11,49 +11,1061 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-char lookahead; /* Excepcionalmente variavel global */
+char *lookahead; /* Excepcionalmente variavel global */
 
-int match(char t, char palavra[], int *pos);
-int parseInit();
-int parseType();
-int parseIdentifier();
-int parseOpenParentesis();
-int parseFuncParameters();
-int parseCloseParantesis();
+int match(char *t, char *palavra, int *counter);
+// int parseInit();
+// int parseType();
+// int parseIdentifier();
+// int parseOpenParentesis();
+// int parseFuncParameters();
+// int parseCloseParantesis();
+int parseVoid(char *palavra, int *counter);
 
-int parseInit()
+char *scanner(char *lexema, int *counter);
+
+int match(char *t, char *palavra, int *counter)
 {
-  if (lookahead == 'SEMIC')
-  {
-    parseIdentifier();
-  }
-  parseType();
-  parseIdentifier();
-  parseOpenParentesis();
-  parseFuncParameters();
-  parseCloseParantesis();
-  parseOpenBrackets();
-}
+  printf("lookahead: %s\n", lookahead);
+  printf("match param: %s\n", t);
+  printf("match param: %i\n", *counter);
 
-int match(char t, char palavra[], int *pos)
-{
   if (lookahead == t)
   {
-    lookahead = palavra[++(*pos)];
+    printf("Before new scanner...\n");
+    lookahead = scanner(palavra, counter);
+    printf("After new scanner...\n");
+    printf("Match!\n");
     return (1);
   }
+  printf("Not Match!\n");
   return (0);
+}
+
+int parseVoid(char *palavra, int *counter) {
+  match("VOID", palavra, counter);
+  match("IDENTIFICADOR", palavra, counter);
 }
 
 int main()
 {
 
-  FILE *arq = fopen("entrada.txt", "r");
-  if (arq == NULL)
+  FILE *entrada;
+  entrada = fopen("entrada.txt", "r");
+
+  if (entrada == NULL)
   {
-    fprintf(stderr, "Erro ao tentar abrir o aquivo \"%s\"", "entrada.txt");
-    return -1;
+    printf("Error opening file.\n");
+    return 1;
   }
 
+  // Obtém o tamanho do arquivo
+  fseek(entrada, 0L, SEEK_END);
+  int tamanho = ftell(entrada);
+  rewind(entrada);
+
+  // Lê o conteúdo do arquivo para uma variável "palavra"
+  char *palavra = (char *)malloc(sizeof(char) * (tamanho + 1));
+  fread(palavra, sizeof(char), tamanho, entrada);
+  palavra[tamanho] = '\0';
+
+  // Fecha o arquivo "entrada.txt"
+  fclose(entrada);
+
+  int counter = 0;
+  printf("OPA!\n");
+  lookahead = scanner(palavra, &counter);
+  printf("OPA 2!\n");
+  parseVoid(palavra, &counter);
+  
   return 0;
+}
+
+char *scanner(char *lexema, int *counter)
+{
+  char digit = lexema[*counter];
+
+q0:
+  if (isdigit(digit))
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q99;
+  }
+  if (digit == '*')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q73;
+  }
+  if (digit == '+')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q69;
+  }
+  if (digit == '-')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q71;
+  }
+  if (digit == '/')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q75;
+  }
+  if (digit == ';')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q67;
+  }
+  if (digit == ',')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q65;
+  }
+  if (digit == '(')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q61;
+  }
+  if (digit == ')')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q63;
+  }
+  if (digit == '{')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q59;
+  }
+  if (digit == '}')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q57;
+  }
+  if (digit == '_')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q93;
+  }
+  if (digit == '=')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q89;
+  }
+  if (digit == '<')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q79;
+  }
+  if (digit == '>')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q85;
+  }
+  if (digit == 's')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q51;
+  }
+  if (digit == 'i')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q45;
+  }
+  if (digit == 'b')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q40;
+  }
+  if (digit == 'd')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q37;
+  }
+  if (digit == 't')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q32;
+  }
+  if (digit == 'f')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q26;
+  }
+  if (digit == 'e')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q21;
+  }
+  if (digit == 'w')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q15;
+  }
+  if (digit == 'v')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q10;
+  }
+  if (digit == 'p')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q1;
+  }
+  else
+  {
+    goto erro;
+  }
+q1:
+  if (digit == 'r')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q2;
+  }
+q2:
+  if (digit == 'i')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q3;
+  }
+  if (digit == 'o')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q7;
+  }
+q3:
+  if (digit == 'n')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q4;
+  }
+q4:
+  if (digit == 't')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q5;
+  }
+q5:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q6;
+  }
+  else
+  {
+    goto erro;
+  }
+q6: // print
+  return "PRINT";
+q7:
+  if (digit == 'c')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q8;
+  }
+q8:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q9;
+  }
+  else
+  {
+    goto erro;
+  }
+q9: // proc
+  return "PROC";
+q10:
+  if (digit == 'o')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q11;
+  }
+q11:
+  if (digit == 'i')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q12;
+  }
+q12:
+  if (digit == 'd')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q13;
+  }
+q13:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q14;
+  }
+  else
+  {
+    goto erro;
+  }
+q14: // void
+  return "VOID";
+q15:
+  if (digit == 'h')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q16;
+  }
+q16:
+  if (digit == 'i')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q17;
+  }
+q17:
+  if (digit == 'l')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q18;
+  }
+q18:
+  if (digit == 'e')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q19;
+  }
+q19:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q20;
+  }
+  else
+  {
+    goto erro;
+  }
+q20: // while
+  return "WHILE";
+q21:
+  if (digit == 'l')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q22;
+  }
+q22:
+  if (digit == 's')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q23;
+  }
+q23:
+  if (digit == 'e')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q24;
+  }
+q24:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q25;
+  }
+  else
+  {
+    goto erro;
+  }
+q25: // else
+  return "ELSE";
+q26:
+  if (digit == 'a')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q27;
+  }
+q27:
+  if (digit == 'l')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q28;
+  }
+q28:
+  if (digit == 's')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q29;
+  }
+q29:
+  if (digit == 'e')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q30;
+  }
+q30:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q31;
+  }
+  else
+  {
+    goto erro;
+  }
+q31: // false
+  return "FALSE";
+q32:
+  if (digit == 'r')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q33;
+  }
+q33:
+  if (digit == 'u')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q34;
+  }
+q34:
+  if (digit == 'e')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q35;
+  }
+q35:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q36;
+  }
+  else
+  {
+    goto erro;
+  }
+q36: // true
+  return "TRUE";
+q37:
+  if (digit == 'o')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q38;
+  }
+q38:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q39;
+  }
+  else
+  {
+    goto erro;
+  }
+q39: // do
+  return "DO";
+q40:
+  if (digit == 'o')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q41;
+  }
+q41:
+  if (digit == 'o')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q42;
+  }
+q42:
+  if (digit == 'l')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q43;
+  }
+q43:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q44;
+  }
+  else
+  {
+    goto erro;
+  }
+q44: // bool
+  return "BOOL";
+q45:
+  if (digit == 'n')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q48;
+  }
+  if (digit == 'f')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q46;
+  }
+q46:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q47;
+  }
+  else
+  {
+    goto erro;
+  }
+q47: // if
+  return "IF";
+q48:
+  if (digit == 't')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q49;
+  }
+q49:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q50;
+  }
+  else
+  {
+    goto erro;
+  }
+q50: // int
+  return "INT";
+q51:
+  if (digit == 'e')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q52;
+  }
+q52:
+  if (digit == 'm')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q53;
+  }
+q53:
+  if (digit == 'i')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q54;
+  }
+q54:
+  if (digit == 'c')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q55;
+  }
+q55:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q56;
+  }
+  else
+  {
+    goto erro;
+  }
+q56: // semic
+  return "SEMIC";
+q57:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q58;
+  }
+  else
+  {
+    goto erro;
+  }
+q58: // }
+  return "FECHA_CHAVES";
+q59:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q60;
+  }
+  else
+  {
+    goto erro;
+  }
+q60: // {
+  return "ABRE_CHAVES";
+q61:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q62;
+  }
+  else
+  {
+    goto erro;
+  }
+q62: // (
+  return "ABRE_PARENTESIS";
+q63:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q64;
+  }
+  else
+  {
+    goto erro;
+  }
+q64: // )
+  return "FECHA_PARENTESIS";
+q65:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q66;
+  }
+  else
+  {
+    goto erro;
+  }
+q66: // ,
+  return "VIRGULA";
+q67:
+  if (digit == ' ')
+  {
+    goto q68;
+  }
+  else
+  {
+    goto erro;
+  }
+q68: // ;
+  return "PONTO_E_VIRGULA";
+q69:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q70;
+  }
+  else
+  {
+    goto erro;
+  }
+q70: // +
+  return "OPR_ADICAO";
+q71:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q72;
+  }
+  else
+  {
+    goto erro;
+  }
+q72: // -
+  return "OPR_SUBTRACAO";
+q73:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q74;
+  }
+  else
+  {
+    goto erro;
+  }
+q74: // *
+  return "OPR_MULTIPLICACAO";
+q75:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q76;
+  }
+  if (digit == '*')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q77;
+  }
+  else
+  {
+    goto erro;
+  }
+q76: // /
+  return "OPR_DIVISAO";
+q77:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q78;
+  }
+  else
+  {
+    goto erro;
+  }
+q78:
+  if (
+      isalpha(digit) ||
+      isalnum(digit) ||
+      digit == '(' ||
+      digit == ')' ||
+      digit == '*' ||
+      digit == '+' ||
+      digit == '-' ||
+      digit == ',' ||
+      digit == '/' ||
+      digit == ';' ||
+      digit == '<' ||
+      digit == '=' ||
+      digit == '>' ||
+      digit == '_' ||
+      digit == '}' ||
+      digit == '{' ||
+      digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q94;
+  }
+q79:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q80;
+  }
+  if (digit == '=')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q83;
+  }
+  if (digit == '>')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q81;
+  }
+  else
+  {
+    goto erro;
+  }
+q80: // <
+  return "MENOR_QUE";
+q81:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q82;
+  }
+  else
+  {
+    goto erro;
+  }
+q82: // <>
+  return "DIFERENCA";
+q83:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q84;
+  }
+  else
+  {
+    goto erro;
+  }
+q84: // <=
+  return "MENOR_OU_IGUAL_QUE";
+q85:
+  if (digit == '=')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q87;
+  }
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q86;
+  }
+  else
+  {
+    goto erro;
+  }
+q86: // >
+  return "MAIOR_QUE";
+q87:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q88;
+  }
+  else
+  {
+    goto erro;
+  }
+q88: // >=
+  return "MAIOR_OU_IGUAL_QUE";
+q89:
+  if (digit == '=')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q91;
+  }
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q90;
+  }
+  else
+  {
+    goto erro;
+  }
+q90: // =
+  return "ATRIBUICAO";
+q91:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q92;
+  }
+  else
+  {
+    goto erro;
+  }
+q92: // ==
+  return "IGUALDADE";
+q93:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q98;
+  }
+  if (isalpha(digit))
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q93;
+  }
+  else
+  {
+    goto erro;
+  }
+q94:
+  if (digit == '*' || digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q95;
+  }
+  if (
+      isalpha(digit) ||
+      isalnum(digit) ||
+      digit == '(' ||
+      digit == ')' ||
+      digit == '+' ||
+      digit == '-' ||
+      digit == ',' ||
+      digit == ';' ||
+      digit == '<' ||
+      digit == '=' ||
+      digit == '>' ||
+      digit == '_' ||
+      digit == '}' ||
+      digit == '{' ||
+      digit == '/')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q78;
+  }
+q95:
+  if (digit == '*' || digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q96;
+  }
+  if (
+      isalpha(digit) ||
+      isalnum(digit) ||
+      digit == '(' ||
+      digit == ')' ||
+      digit == '+' ||
+      digit == '-' ||
+      digit == ',' ||
+      digit == ';' ||
+      digit == '<' ||
+      digit == '=' ||
+      digit == '>' ||
+      digit == '_' ||
+      digit == '}' ||
+      digit == '{' ||
+      digit == '*' ||
+      digit == '/')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q94;
+  }
+  else
+  {
+    goto erro;
+  }
+q96:
+  if (digit == '*')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q96;
+  }
+  if (digit == '/')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q97;
+  }
+  if (
+      isalpha(digit) ||
+      isalnum(digit) ||
+      digit == '(' ||
+      digit == ')' ||
+      digit == '+' ||
+      digit == '-' ||
+      digit == ',' ||
+      digit == ';' ||
+      digit == '<' ||
+      digit == '=' ||
+      digit == '>' ||
+      digit == '_' ||
+      digit == '}' ||
+      digit == '{' ||
+      digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q95;
+  }
+  else
+  {
+    goto erro;
+  }
+q97:
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q101;
+  }
+  else
+  {
+    goto erro;
+  }
+q98: // Identificador
+  return "IDENTIFICADOR";
+q99:
+  if (isdigit(digit))
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q99;
+  }
+  if (digit == ' ')
+  {
+    (*counter)++;
+    digit = lexema[*counter];
+    goto q100;
+  }
+  else
+  {
+    goto erro;
+  }
+q100: // Digitos
+  return "NUM";
+q101: // Comentario
+  return "COMENTARIO";
+erro: // Erro léxico
+  return "ERRO LEXICO";
 }
